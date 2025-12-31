c066b4e395468e3794f3fed29d2772a7f02c39ab
shakibalhassan08 <shakib.mabupbasha@wizklub.com>
2025-12-31 14:29:20 +0530

    server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
 const path = require("path");
-require("dotenv").config();
 
 const app = express();
 
@@ -119,3 +118,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
+
